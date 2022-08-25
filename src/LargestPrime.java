acda194d5ffc391e1786ee4be4e7109a80b94ca6
Mr.M.V.BALA <mvbalakrishnan95@gmail.com>
2022-08-25 20:18:47 +0530

    it is udemy exercise.

--- src/LargestPrime.java
+++ src/LargestPrime.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class LargestPrime {
+public class LargestPrime {
+    public static int getLargestPrime(int number) {
+        if (number < 2) {
+            return -1;
+        }else {
+            for (int i = 2; i < number; i++) {
+                if ((number % i) == 0) {
+                    number /= i;
+                    i--;
+                }
+            }
+            return number;
+        }
+    }
 }
